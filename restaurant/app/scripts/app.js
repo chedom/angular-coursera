69df4f665f96586bd3b519420809359bc93a009d
Vasyl Domanchuk <domanchukits@gmail.com>
2016-07-05 13:21:02 +0300

    done filter

--- restaurant/app/scripts/app.js
+++ restaurant/app/scripts/app.js
@@ -1,5 +1,26 @@
 var app = angular.module('confusionApp', []);
 app.controller('menuController', function() {
+
+    this.tab = 1;
+    this.filterText = '';
+
+    this.select = function(setTab) {
+        this.tab = setTab;
+        if (setTab === 2) {
+            this.filterText = 'appetizers';
+        } else if (setTab === 3 ) {
+            this.filterText = 'mains';
+        } else if (setTab === 4) {
+            this.filterText = 'dessert';
+        } else {
+            this.filterText = '';
+        }
+    };
+
+    this.isSelected = function(checkTab) {
+        return checkTab === this.tab;
+    };
+
     this.dishes = [
         {
             name       : 'Uthapizza',
@@ -22,7 +43,7 @@ app.controller('menuController', function() {
         {
             name       : 'elaicheesecake',
             image      : 'images/elaicheesecake.png',
-            category   : 'mains',
+            category   : 'appetizers',
             label      : 'Hot',
             price      : '9.44',
             description: 'A unique combination of Indian Uthappam and Italian pizza',
@@ -31,7 +52,7 @@ app.controller('menuController', function() {
         {
             name       : 'Vadonut',
             image      : 'images/vadonut.png',
-            category   : 'mains',
+            category   : 'desserts',
             label      : '',
             price      : '41.12',
             description: 'A unique combination of Indian Uthappam and Italian pizza',
