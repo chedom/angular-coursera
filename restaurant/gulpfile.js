bdcc9e0ac59e68586e88fad0562d94b6478e4090
Vasyl Domanchuk <domanchukits@gmail.com>
2016-07-17 20:26:12 +0300

    init contacts html

--- restaurant/gulpfile.js
+++ restaurant/gulpfile.js
@@ -24,7 +24,7 @@ gulp.task('jshint', function() {
 
 //Usemin
 gulp.task('usemin',['jshint'], function () {
-  return gulp.src('./app/menu.html')
+  return gulp.src('./app/contacts.html')
       .pipe(usemin({
         css:[minifycss(),rev()],
         js: [ngAnnotate(), uglify(),rev()]
@@ -77,7 +77,7 @@ gulp.task('browser-sync', ['default'], function () {
    browserSync.init(files, {
       server: {
          baseDir: "dist",
-         index: "menu.html"
+         index: "contacts.html"
       }
    });
         // Watch any files in dist/, reload on change
