e62a823628e6f0b822c12445ad201265cee39c15
Vasyl Domanchuk <domanchukits@gmail.com>
2016-07-23 19:17:22 +0300

    add route config

--- restaurant/app/scripts/app.js
+++ restaurant/app/scripts/app.js
@@ -1,3 +1,22 @@
 'use strict';
 
-var app = angular.module('confusionApp', []);
+var app = angular.module('confusionApp', ['ngRoute'])
+	.config(function($routeProvider) {
+		$routeProvider
+			.when('/contactus', {
+				templateUrl: 'templates/contacts.html',
+				controller: 'ContactController'
+			})
+
+			.when('/menu', {
+				templateUrl: 'templates/menu.html',
+				controller: 'MenuController'
+			})
+
+			.when('/menu/:id', {
+				templateUrl: 'templates/dishdetail.html',
+				controller: 'DishDetailController'
+			})
+
+			.otherwise('/contactus');
+	})
