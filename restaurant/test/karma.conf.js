7eeeda315b10921fc166283330c3dd016c83e7e2
Vasyl Domanchuk <domanchukits@gmail.com>
2016-07-24 21:12:45 +0300

    done unit test

--- restaurant/test/karma.conf.js
+++ restaurant/test/karma.conf.js
@@ -1,13 +1,13 @@
 module.exports = function(config) {
 	config.set({
-		basePath: './',
+		basePath: '../',
 
 		frameworks: ['jasmine'],
 
 		files: [
 			'bower_components/angular/angular.js',
 			'bower_components/angular-resource/angular-resource.js',
-			'bower_components/angular-ui-router/angular-ui-router.js',
+			'bower_components/angular-ui-router/release/angular-ui-router.js',
 			'bower_components/angular-mocks/angular-mocks.js',
 			'app/scripts/*.js',
 			'test/**/*.js'
@@ -16,6 +16,52 @@ module.exports = function(config) {
 		exclude: [
 			'test/protractor.conf.js',
 			'test/e2e/*.js'
-		]
+		],
+
+		preprocessors: {},
+
+		reporters: ['progress'],
+
+		port: 9000,
+
+		colors: true,
+		logLevel: config.LOG_INFO,
+
+		autoWatch: true,
+
+		 // start these browsers
+	    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+	     browsers: ['Chrome','PhantomJS', 'PhantomJS_custom'],
+
+	    // you can define custom flags
+	    customLaunchers: {
+	      'PhantomJS_custom': {
+	        base: 'PhantomJS',
+	        options: {
+	          windowName: 'my-window',
+	          settings: {
+	            webSecurityEnabled: false
+	          },
+	        },
+	        flags: ['--load-images=true'],
+	        debug: true
+	      }
+	    },
+
+	    phantomjsLauncher: {
+	      // Have phantomjs exit if a ResourceError is encountered (useful if karma exits without killing phantom)
+	      exitOnResourceError: true
+	    },
+
+	    // Continuous Integration mode
+	    // if true, Karma captures browsers, runs the tests and exits
+	    singleRun: false,
+
+	    // Concurrency level
+	    // how many browser should be started simultaneous
+	    concurrency: Infinity
+
+
+
 	})
 }
